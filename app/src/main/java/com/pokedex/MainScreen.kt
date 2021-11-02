6d784529745af87943ef5c43483120f1d3570bc6
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-02 12:47:54 +0100

    nearly finished list

--- app/src/main/java/com/pokedex/MainScreen.kt
+++ app/src/main/java/com/pokedex/MainScreen.kt
@@ -1,27 +1,61 @@
 package com.pokedex
 
+import android.content.res.Resources
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.pokedex.components.BottomNavBar
+import com.pokedex.components.Navigation
 import com.pokedex.components.TopBar
 
+@ExperimentalAnimationApi
 @Composable
 fun MainScreen(content: @Composable() () -> Unit) {
-    val navController = rememberNavController()
-
+    val appState = rememberMyAppState()
     Scaffold(
+        scaffoldState = appState.scaffoldState,
         topBar = { TopBar() },
-        bottomBar = { BottomNavBar(navController) },
-    ) {
-        content()
-        //        Navigation(navController)
+        bottomBar = { BottomNavBar(appState.navController) },
+    ) { innerPadding ->
+        // Apply the padding globally to the whole BottomNavScreensController
+        BoxWithConstraints(modifier = Modifier.padding(innerPadding)) {
+//        content()
+            Navigation(appState.navController, maxWidth = constraints.maxWidth)
+        }
+
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun PreviewWithMainScreen(content: @Composable() () -> Unit) {
     MainScreen {
         content()
     }
+}
+
+class AppState(
+    val scaffoldState: ScaffoldState,
+    val navController: NavHostController,
+    private val resources: Resources,
+)
+
+@ExperimentalAnimationApi
+@Composable
+fun rememberMyAppState(
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    navController: NavHostController = rememberAnimatedNavController(),
+    resources: Resources = LocalContext.current.resources,
+) = remember(scaffoldState, navController, resources) {
+    AppState(scaffoldState, navController, resources)
 }
