ba979824f62ae579090007f8db365952dc9bcaa6
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-11-03 13:12:45 +0100

    restructured app

--- app/src/main/java/com/pokedex/models/Ability.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("EXPERIMENTAL_API_USAGE", "EXPERIMENTAL_UNSIGNED_LITERALS")
-
-package com.pokedex.models
-
-data class Ability(
-        val slot: UInt,
-        val name: String,
-)
