b9dcf5baea2d1457576c6755f64b76a43d58f546
abdo643 <abou.shehata643@yahoo.de>
2021-11-07 21:34:59 +0100

    fixed Navigation

--- app/src/main/kotlin/com/shehata/pokedex/NavigationItem.kt
+++ app/src/main/kotlin/com/shehata/pokedex/NavigationItem.kt
@@ -13,10 +13,14 @@ sealed class BottomNavigationItem(
     object PokemonList : BottomNavigationItem("pokemon-list", "Pokemon", R.drawable.ic_pokeball)
     object Profile : BottomNavigationItem("profile", "Profile", R.drawable.ic_outline_person_24)
     object Favourites :
-        BottomNavigationItem("favourites", "Favourites", R.drawable.ic_outline_person_24)
+        BottomNavigationItem("favourites", "Favourites", R.drawable.ic_baseline_favorite_border_24)
 }
 
 sealed class NavigationItem(val route: String, val arguments: List<NamedNavArgument>) {
+    object PokemonList : BottomNavigationItem("pokemon-list", "Pokemon", R.drawable.ic_pokeball)
+    object Profile : BottomNavigationItem("profile", "Profile", R.drawable.ic_outline_person_24)
+    object Favourites :
+        BottomNavigationItem("favourites", "Favourites", R.drawable.ic_baseline_favorite_border_24)
     object PokemonDetails :
         NavigationItem(
             route = "pokemon-details",
--- app/src/main/kotlin/com/shehata/pokedex/components/Navigation.kt
+++ app/src/main/kotlin/com/shehata/pokedex/components/Navigation.kt
@@ -6,21 +6,28 @@ import android.util.Log
 import androidx.compose.animation.*
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.navigation
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.shehata.pokedex.BottomNavigationItem
 import com.shehata.pokedex.NavigationItem
 import com.shehata.pokedex.screens.pokemondetails.PokemonDetailsScreen
+import com.shehata.pokedex.screens.pokemondetails.PokemonDetailsState
 import com.shehata.pokedex.screens.pokemondetails.PokemonDetailsViewModel
 import com.shehata.pokedex.screens.pokemonlist.PokemonListScreen
+import com.shehata.pokedex.screens.pokemonlist.PokemonListState
 import com.shehata.pokedex.screens.pokemonlist.PokemonListViewModel
+import com.shehata.pokedex.screens.profile.ProfileScreen
 
 @Composable
 fun Navigation(
@@ -28,14 +35,24 @@ fun Navigation(
     maxWidth: Int,
     start: String = BottomNavigationItem.PokemonList.route
 ) {
+
     AnimatedNavHost(navController, start) {
+        val width = maxWidth / 2
         addPokemonListScreen(
             navController,
-            maxWidth / 2
+            width
         )
         addPokemonDetailsScreen(
             navController,
-            maxWidth / 2
+            width
+        )
+        addFavouritesScreen(
+            navController,
+            width
+        )
+        addProfileScreen(
+            navController,
+            width
         )
     }
 }
@@ -46,43 +63,54 @@ fun NavGraphBuilder.addPokemonListScreen(
 ) {
     composable(
         BottomNavigationItem.PokemonList.route,
-        enterTransition = { initial, _ ->
-            when (initial.destination.route) {
-                NavigationItem.PokemonDetails.route ->
+        enterTransition = {
+            when (initialState.destination.route) {
+                NavigationItem.PokemonDetails.route -> {
+                    Log.i("POKEMON", "${true}")
                     slideInHorizontally(
-                        initialOffsetX = { width / 2 },
+                        initialOffsetX = { width },
                         animationSpec = tween(300)
                     ) + fadeIn(animationSpec = tween(300))
+                }
                 else -> null
             }
         },
-        popEnterTransition = { initial, _ ->
-            slideInHorizontally(
-                initialOffsetX = { -(width / 2) },
-                animationSpec = tween(
-                    durationMillis = 300,
-                    easing = FastOutSlowInEasing
-                )
-            ) + fadeIn(animationSpec = tween(300))
+        popEnterTransition = {
+            when (initialState.destination.route?.contains(NavigationItem.PokemonDetails.route)) {
+                true -> {
+                    slideInHorizontally(
+                        initialOffsetX = { -width },
+                        animationSpec = tween(
+                            durationMillis = 300,
+                            easing = FastOutSlowInEasing
+                        )
+                    ) + fadeIn(animationSpec = tween(300))
+                }
+                else -> null
+            }
+
         },
-        exitTransition = { _, target ->
-            when (target.destination.route?.contains(NavigationItem.PokemonDetails.route)) {
-                 true -> {
-                     slideOutHorizontally(
-                         targetOffsetX = { -(width / 2) },
-                         animationSpec = tween(
-                             durationMillis = 300,
-                             easing = FastOutSlowInEasing
-                         )
-                     ) + fadeOut(animationSpec = tween(300))
+        exitTransition = {
+            when (targetState.destination.route?.contains(NavigationItem.PokemonDetails.route)) {
+                true -> {
+                    slideOutHorizontally(
+                        targetOffsetX = { -width },
+                        animationSpec = tween(
+                            durationMillis = 300,
+                            easing = FastOutSlowInEasing
+                        )
+                    ) + fadeOut(animationSpec = tween(300))
                 }
                 else -> null
             }
         },
     ) {
-        val viewModel = PokemonListViewModel(navController)
+        val vm: PokemonListViewModel = viewModel()
+        val uiState by vm.uiState.observeAsState(PokemonListState())
+
         PokemonListScreen(
-            viewModel,
+            uiState,
+            fetchPokemon = vm::fetchPokemon,
             navigateToDetailScreen = { pokemonId ->
                 navController.navigate("${NavigationItem.PokemonDetails.route}/$pokemonId")
             }
@@ -97,28 +125,33 @@ fun NavGraphBuilder.addPokemonDetailsScreen(
     composable(
         route = NavigationItem.PokemonDetails.route + "/{pokemonId}",
         arguments = NavigationItem.PokemonDetails.arguments,
-        enterTransition = { _, _ ->
-            slideInHorizontally(
-                initialOffsetX = { width / 2 },
-                animationSpec = tween(
-                    durationMillis = 300,
-                    easing = FastOutSlowInEasing
-                )
-            ) + fadeIn(animationSpec = tween(300))
+        enterTransition = {
+            when (initialState.destination.route) {
+                BottomNavigationItem.PokemonList.route -> {
+                    slideInHorizontally(
+                        initialOffsetX = { width },
+                        animationSpec = tween(
+                            durationMillis = 300,
+                            easing = FastOutSlowInEasing
+                        )
+                    ) + fadeIn(animationSpec = tween(300))
+                }
+                else -> null
+            }
         },
-        exitTransition = { _, target ->
-            when (target.destination.route) {
+        exitTransition = {
+            when (targetState.destination.route) {
                 BottomNavigationItem.PokemonList.route ->
                     slideOutHorizontally(
-                        targetOffsetX = { -(width / 2) },
+                        targetOffsetX = { -width },
                         animationSpec = tween(300)
                     ) + fadeOut(animationSpec = tween(300))
                 else -> null
             }
         },
-        popExitTransition = { _, target ->
+        popExitTransition = {
             slideOutHorizontally(
-                targetOffsetX = { width / 2 },
+                targetOffsetX = { width },
                 animationSpec = tween(
                     durationMillis = 300,
                     easing = FastOutSlowInEasing
@@ -127,10 +160,40 @@ fun NavGraphBuilder.addPokemonDetailsScreen(
         }
     ) {
         val vm: PokemonDetailsViewModel = viewModel()
-        val state by vm.uiState.observeAsState()
+        val state by vm.uiState.observeAsState(PokemonDetailsState(null))
 
-        state?.let {
-            PokemonDetailsScreen(it)
-        }
+        PokemonDetailsScreen(
+            state,
+            navigateUp = {
+                navController.navigateUp()
+            }
+        )
     }
 }
+
+fun NavGraphBuilder.addFavouritesScreen(
+    navController: NavController,
+    width: Int,
+) {
+    composable(
+        route = BottomNavigationItem.Favourites.route,
+    ) {
+        Text(
+            "Favourites",
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+
+fun NavGraphBuilder.addProfileScreen(
+    navController: NavController,
+    width: Int,
+) {
+    composable(
+        route = BottomNavigationItem.Profile.route,
+    ) {
+        ProfileScreen(navController = navController)
+    }
+}
+
