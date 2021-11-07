45eb4cbac4352ea41cdad2bc958cbecf8318c93e
abdo643 <abou.shehata643@yahoo.de>
2021-11-07 21:36:02 +0100

    added for animation

--- app/src/main/kotlin/com/shehata/pokedex/screens/profile/Profile.kt
+++ app/src/main/kotlin/com/shehata/pokedex/screens/profile/Profile.kt
@@ -1,29 +1,30 @@
 @file:OptIn(ExperimentalAnimationApi::class)
+
 package com.shehata.pokedex.screens.profile
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.shehata.pokedex.PreviewWithMainScreen
 
 @Composable
 fun ProfileScreen(navController: NavController) {
     Text(
         "Profile",
-        fontSize = 30.sp
+        fontSize = 30.sp,
+        modifier = Modifier.fillMaxSize()
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ProfileScreenPreview() {
-    PreviewWithMainScreen {
-        ProfileScreen(
-            rememberNavController()
-        )
-    }
+    ProfileScreen(
+        rememberNavController()
+    )
 }
