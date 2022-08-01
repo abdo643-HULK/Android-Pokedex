09a10e4c4f9cd1119bfd5ce805bf179105de99bf
abdo643 <abou.shehata643@yahoo.de>
2022-08-01 07:46:32 +0200

    unit test

--- app/src/test/kotlin/com/shehata/pokedex/ExampleUnitTest.kt
+++ app/src/test/kotlin/com/shehata/pokedex/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
-package com.pokedex
+package com.shehata.pokedex
 
+import com.shehata.pokedex.models.Stats
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private lateinit var stats: Stats
+
+    @Before
+    fun setUp() {
+        stats = Stats(hp = 1u, attack = 1u, defense = 1u, speed = 1u, specialAttack = 2u, specialDefense = 1u)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testStatsTotal() {
+        assertEquals(stats.total, 7)
+        assertNotEquals(stats.total, 8)
+    }
+
+    @Test
+    fun testStatsBiggest() {
+        assertEquals(stats.biggestStat, 2)
+        assertNotEquals(stats.biggestStat, 3)
     }
 }
